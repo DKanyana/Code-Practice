0e394afeb01b6d43c27cd46c8095236b341d902a
MyMishi <34791009+MyMishi@users.noreply.github.com>
2018-07-09 20:44:15 -0700

    Update TwoToOne.py

--- TwoToOne.py
+++ TwoToOne.py
@@ -1,6 +1,8 @@
-Problem - 
+===================
+Problem -
 Take 2 strings s1 and s2 including only letters from ato z. 
 Return a new sorted string, the longest possible, containing distinct letters.
+===================
 
 Solution - 
 def longest(s1, s2):
