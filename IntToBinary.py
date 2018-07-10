b41574dee418c98f36a72ad588de834ad99a8eb4
MyMishi <34791009+MyMishi@users.noreply.github.com>
2018-07-10 16:26:11 -0700

    Create IntToBinary.py

---
+++ IntToBinary.py
@@ -0,0 +1,17 @@
+"""
+
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+"""
+
+def add_binary(a,b):
+    add = a+b
+    result = ""
+    while add > 0:
+        remainder =add%2
+        add = add//2
+        result += str(remainder)
+    return result[::-1]
+        
